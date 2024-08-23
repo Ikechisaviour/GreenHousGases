9edc6b12ed5674c07f8d6b956986216e385bca21
Ikechisaviour <43084681+Ikechisaviour@users.noreply.github.com>
2024-08-24 02:02:10 +0900

    Update ghg.sol

--- ghg.sol
+++ ghg.sol
@@ -1,11 +1,18 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity 0.8.10;
+pragma abicoder v2;
+
+// pragma experimental ABIEncoderV2;
+
 contract ghg{
 
     struct company{
         string name;
         uint addition;
         uint subtraction;
+        uint paidToCompany;
+        uint paidFromCompany;
         uint netEmission;
         bool aboveZero;
         address county;
@@ -16,12 +23,20 @@ contract ghg{
         string name;
         uint addition;
         uint subtraction;
+        uint paidToCounty;
+        uint paidFromCounty;
         uint netEmission;
         bool aboveZero;
+        string [] companies;
         
     }
 
-    string public empty;
+    uint public trace1;
+    uint public trace2;
+
+    address authority = msg.sender;
+
+    string empty;
     uint buyAmount = 11;
     uint sellAmount = 9;
 
@@ -30,23 +45,37 @@ contract ghg{
     mapping (address => string) used;
     mapping (string => bool) usedName;
 
-    function register(address _address, string memory _institution, string memory _name) external {
+    function countyRegister(address _address, string memory _name) external {
+        //should only be called by the authority
         bytes32 first = keccak256(abi.encodePacked(used[_address]));
         bytes32 second = keccak256(abi.encodePacked(empty));
+        //# require(msg.sender == authority, "You are not the authority in this smart contract");
         require(first == second, "address already registered");
         require(usedName[_name] == false, "this name has been used before");
-        if(keccak256(abi.encodePacked(_institution)) == keccak256(abi.encodePacked("company"))){
-            companys[_address].name = _name;
-            used[_address] = "company";
-        }else if(keccak256(abi.encodePacked(_institution)) == keccak256(abi.encodePacked("county"))){
-            countys[_address].name = _name;
-            used[_address] = "county";
-        }
+            
+        countys[_address].name = _name;
+        used[_address] = "county";
         usedName[_name] = true;
 
     }
 
+    function companyRegister(address _address, string memory _name) external {
+        //should only be called by the county
+        bytes32 first = keccak256(abi.encodePacked(used[_address]));
+        bytes32 second = keccak256(abi.encodePacked(empty));
+        // checkin if the string is empty or assigned an address
+        require(keccak256(abi.encodePacked(used[msg.sender])) == keccak256(abi.encodePacked("county")), "You are not a county");
+        require(first == second, "address already registered");
+        require(usedName[_name] == false, "this name has been used before");
+        companys[_address].name = _name;
+        countys[msg.sender].companies.push(_name);
+        companys[_address].county = msg.sender;
+        used[_address] = "company";
+        usedName[_name] = true;
+    }
+
     function companyUpdate(uint _ghgQty, bool _ghgadding) external {
+        //should only be called by a company
         require(keccak256(abi.encodePacked(used[msg.sender])) == keccak256(abi.encodePacked("company")), "the address is not a company");
         address currentCounty = companys[msg.sender].county;
         if(_ghgadding == true){
@@ -61,43 +90,50 @@ contract ghg{
     }
 
     function recalculateGhg(address _county) private {
-        if(companys[msg.sender].addition > companys[msg.sender].subtraction){
-            companys[msg.sender].netEmission = companys[msg.sender].addition -companys[msg.sender].subtraction;
+        if((companys[msg.sender].addition + companys[msg.sender].paidToCompany) >= (companys[msg.sender].subtraction + companys[msg.sender].paidFromCompany)){
+            companys[msg.sender].netEmission = (companys[msg.sender].addition + companys[msg.sender].paidToCompany) - (companys[msg.sender].subtraction + companys[msg.sender].paidFromCompany);
             companys[msg.sender].aboveZero = true;
-            countys[_county].netEmission = countys[_county].addition - countys[_county].subtraction;
-            countys[_county].aboveZero = true;
         }else if(companys[msg.sender].addition < companys[msg.sender].subtraction){
-            companys[msg.sender].netEmission = companys[msg.sender].subtraction -companys[msg.sender].addition;
+            companys[msg.sender].netEmission = (companys[msg.sender].subtraction + companys[msg.sender].paidFromCompany) - (companys[msg.sender].addition + companys[msg.sender].paidToCompany);
             companys[msg.sender].aboveZero = false;
-            countys[_county].netEmission = countys[_county].subtraction - countys[_county].addition;
+        }else {}
+
+        if((countys[_county].addition + countys[_county].paidToCounty) > (countys[_county].subtraction + countys[_county].paidFromCounty)){            
+            countys[_county].netEmission = (countys[_county].addition + countys[_county].paidToCounty) - (countys[_county].subtraction + countys[_county].paidFromCounty);
+            countys[_county].aboveZero = true;
+        }else if((countys[_county].addition + countys[_county].paidToCounty) < (countys[_county].subtraction + countys[_county].paidFromCounty)){            
+            countys[_county].netEmission = (countys[_county].subtraction + countys[_county].paidFromCounty) - (countys[_county].addition + countys[_county].paidToCounty);
             countys[_county].aboveZero = false;
         }else {}
     }
 
     function payForEmission () payable external {
+        //should only be called by a company
         require(buyAmount * companys[msg.sender].netEmission >= msg.value && companys[msg.sender].aboveZero == true, 
         "ghg emission is below zero or amount is more than you owe");
         uint removing =  msg.value/buyAmount;
-        companys[msg.sender].subtraction + removing;
-        address currentCounty = companys[msg.sender].county;
-        countys[currentCounty].subtraction + removing;
-        
+        companys[msg.sender].paidFromCompany = companys[msg.sender].paidFromCompany + removing;
+        address currentCounty = companys[msg.sender].county;     
+        countys[currentCounty].paidFromCounty = countys[currentCounty].paidFromCounty + removing;
         recalculateGhg(currentCounty);
-        
     }
     
     function setBuyAmount(uint _amount) external {
+        //should be called by the authority
         buyAmount = _amount;
     }
 
     function setSellAmount(uint _amount) external {
+        //should be called by the authority
         sellAmount = _amount;
     }
 
     function getpaidForGHGRemoval () payable external {
-        require(companys[msg.sender].aboveZero == true);
+        //should be called by a company
+        require(companys[msg.sender].aboveZero == false, "");
         address payable receiver = payable(msg.sender);
         receiver.transfer(companys[msg.sender].netEmission * sellAmount);
+        companys[msg.sender].paidToCompany = companys[msg.sender].paidToCompany + companys[msg.sender].netEmission;
         companys[msg.sender].netEmission = 0;
     }
 
@@ -121,8 +157,31 @@ contract ghg{
             myNameReturn = companys[_address].name;          
         }else if (keccak256(abi.encodePacked(used[_address])) == keccak256(abi.encodePacked("county"))){
             myNameReturn = countys[_address].name;
+
         }
         return(used[_address], myNameReturn);
     }
-    
+
+    function viewSellPrice () external view returns(uint){
+        return sellAmount;
+    }
+
+    function viewBuyPrice () external view returns(uint){
+        return buyAmount;
+    }
+
+    function viewCompanyDetail() external view returns(company memory){
+        return(companys[msg.sender]);
+    }
+
+    function viewCountyDetail() external view returns(county memory){
+        return(countys[msg.sender]);
+    }
+
+    function viewCompanyCountyName(address _address) external view returns(string memory){
+        return(countys[companys[_address].county].name);
+    }
+      
+    receive() external payable{}
+
 }
